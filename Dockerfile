c9eca11a0949b0b5bcebf685f1f7464d05551f93
TanvirMahfuz <tanvirmahfuz22@gmail.com>
2025-09-22 12:23:37 +0600

    updated ansible config: now it rebuilds docker image on every run.
    updated index.js: now it gets the host ip address and port as well

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,8 @@
 FROM node:20-alpine
-RUN apk add --no-cache git
-
 WORKDIR /app 
 #so its basically like cd app :apurbo
-COPY package*.json ./
-RUN npm install
 COPY . .
+RUN npm install
+
 EXPOSE 3000
 CMD ["sh", "-c", "node index.js"]
--- index.js
+++ index.js
@@ -3,35 +3,42 @@ import os from "os";
 
 const app = express();
 const port = 3000;
-const hostname = os.hostname();
-
+const hostmachine = os.hostname();
+let serverInfo = {};
 const GITHUB_API_URL = "https://api.github.com/repos/TanvirMahfuz/COMMIT-HASH/commits/main";
 
-app.get("/",async (req, res) => {
-    try {
-        const result = await fetch(GITHUB_API_URL)
-        const data = await result.json(0)
-        console.log({
-            msg: "Fetched data successfully",
-            hostname,
-            commitHash: data.sha,
-            commitMessage: data.commit.message
-        })
-        return res.status(200).json({
-            msg: "Fetched data successfully",
-            hostname,
-            commitHash: data.sha,
-            commitMessage: data.commit.message
-        });
-
-    } catch (error) {
-        res.status(500).json({msg:"internal server error",error: error.message})
+function getLocalIP() {
+  const interfaces = os.networkInterfaces();
+  for (const name of Object.keys(interfaces)) {
+    for (const iface of interfaces[name]) {
+      if (iface.family === "IPv4" && !iface.internal) {
+        return iface.address;
+      }
     }
+  }
+  return "127.0.0.1"; 
+}
 
+app.get("/", async (req, res) => {
+  try {
+    const result = await fetch(GITHUB_API_URL);
+    const data = await result.json();
+
+    return res.status(200).json({
+      msg: "Fetched data successfully",
+      hostmachine,
+      server: serverInfo,
+      commitHash: data.sha,
+      commitMessage: data.commit.message
+    });
+
+  } catch (error) {
+    res.status(500).json({ msg: "internal server error", error: error.message });
+  }
 });
 
-const server = app.listen(port, () => {
-  const address = server.address();
-  const host = address.address === "::" ? "localhost" : address.address;
-  console.log(`Server running on http://${hostname}@${host}:${address.port}`);
+app.listen(port, () => {
+  const host = getLocalIP(); // get real IP
+  serverInfo = { host, port };
+  console.log(`Server running on http://${hostmachine}@${host}:${port}`);
 });
